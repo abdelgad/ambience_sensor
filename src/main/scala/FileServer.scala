027d6165e2cb62d33ef7095b632f90148aff5d25
Abdelrahman Gad <abdelgad@hotmail.com>
2024-11-17 01:36:20 +0100

    Add file server

---
+++ src/main/scala/FileServer.scala
@@ -0,0 +1,41 @@
+import akka.actor.Actor
+import java.io.File
+import java.nio.file.{Files, Paths}
+
+case object NotifySynchronization
+case class FileList(files: Set[String])
+case class RequestFile(fileName: String)
+case class FileContent(fileName: String, content: String)
+case object SendHello
+
+class FileServerActor(folderPath: String, clientAddress: String) extends Actor{
+  override def receive: Receive = {
+    case NotifySynchronization =>
+      println("Notifying client to synchronize...")
+      val files = getFileList
+      context.actorSelection(clientAddress) ! FileList(files)
+
+
+    case RequestFile(fileName) =>
+      println(s"Received request to send file: $fileName")
+      readFile(fileName).foreach { content =>
+        sender() ! FileContent(fileName, content)
+      }
+  }
+
+
+  private def getFileList: Set[String] = {
+    new File(folderPath)
+      .listFiles()
+      .filter(_.isFile)
+      .map(_.getName)
+      .toSet
+  }
+
+
+  private def readFile(fileName: String): Option[String] = {
+    val filePath = Paths.get(folderPath, fileName)
+    if (Files.exists(filePath)) Some(new String(Files.readAllBytes(filePath)))
+    else None
+  }
+}
---
+++ src/main/scala/main.scala
@@ -0,0 +1,20 @@
+import akka.actor.{ActorSystem, Props}
+
+object AmbienceSensor extends App {
+
+  val system = ActorSystem("AmbienceSensor")
+
+  // Create Actors
+  val lcdActor = system.actorOf(Props(new LCDActor), "lcdActor")
+  val tempSensor = system.actorOf(Props(new TemperatureSensorActor), "tempSensor")
+  val humiditySensor = system.actorOf(Props(new HumiditySensorActor), "humiditySensor")
+  val lightSensor = system.actorOf(Props(new LightSensorActor), "lightSensor")
+  val webcam = system.actorOf(Props(new WebcamActor), "webcam")
+  val ambienceDiffuserPath = "akka://AmbienceDiffuser@127.0.0.1:25521/user/fileClient"
+  val fileServer = system.actorOf(Props(new FileServerActor("snapshots", ambienceDiffuserPath)), "fileServer")
+  val snapshotManager = system.actorOf(Props(new SnapshotManager(lcdActor, tempSensor, humiditySensor, lightSensor, webcam, fileServer)), "snapshotManager")
+  val forceSensor = system.actorOf(Props(new ForceSensorActor(snapshotManager)), "forceSensor")
+
+  // Start the system
+  snapshotManager ! Start
+}
