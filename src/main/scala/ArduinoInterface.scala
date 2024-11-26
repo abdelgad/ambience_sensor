4c966a6e6c3ba15f4de4e5873664ea4257c57a32
BaBaTi07 <baptiste.moncomble@gmail.com>
2024-11-26 14:50:28 +0100

    update bpm list to int

--- src/main/scala/ArduinoInterface.scala
+++ src/main/scala/ArduinoInterface.scala
@@ -1,13 +1,7 @@
 import jssc.{SerialPort, SerialPortException}
 
 object ArduinoInterface {
-  /**
-   * Fonction bloquante pour récupérer les données de l'Arduino
-   * @param portName Le port série (par ex. "COM5" ou "/dev/ttyUSB0")
-   * @param duration Durée d'acquisition en secondes
-   * @return (BPM, Liste des données sonores)
-   */
-  def collectData(portName: String, duration: Int): (Int, List[Int]) = {
+  def collectData(portName: String, duration: Int): (Int, Int) = {
     val serialPort = new SerialPort(portName)
     try {
       serialPort.openPort()
@@ -60,13 +54,21 @@ object ArduinoInterface {
         }
       }
 
-      (bpm.getOrElse(0), soundData)
+      (bpm.getOrElse(0), averageL(soundData))
     } catch {
       case ex: SerialPortException =>
         println(s"Erreur de communication avec le port série : ${ex.getMessage}")
-        (0, List())
+        (0, 0)
     } finally {
       if (serialPort.isOpened) serialPort.closePort()
     }
+  }
+
+  def averageL(lst: List[Int]): Int = {
+    lst.sum / lst.size
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(collectData("COM3", 5))
   }
 }
