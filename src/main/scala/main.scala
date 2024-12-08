1fe2b290334b1b81b616954e4e5fdb09485e1d37
Pozza Noé <noe.pozza@student.unamur.com>
2024-12-08 17:11:53 +0100

    add assembly

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -20,7 +20,7 @@ object AmbienceSensor extends App {
   val webcam = system.actorOf(Props(new WebcamActor), "webcam")
 
   // TODO : Replace by "akka://AmbienceDiffuser@ambience-diffuser.local:25521/user/fileClient"
-  val ambienceDiffuserPath = "akka://AmbienceDiffuser@192.168.0.10:25521/user/fileClient"
+  val ambienceDiffuserPath = "akka://AmbienceDiffuser@ambiencediffuser.local:25521/user/fileClient"
   val fileServerActor = system.actorOf(Props(new FileServerActor("snapshots", ambienceDiffuserPath)), "fileServer")
 
   val snapshotManager = system.actorOf(Props(new SnapshotManager(lcdActor, tempSensorActor, humiditySensorActor, lightSensorActor, webcam, fileServerActor)), "snapshotManager")
